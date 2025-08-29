dfa51720ba4f81d53c8275bb590e4b1fa7cd7be8
Angel Tatcheff <a.tatcheff@abv.bg>
2025-08-29 12:20:38 +0300

    commited basic test contextLoads

--- src/test/java/com/ridetogether/chat_service/ChatServiceApplicationTests.java
+++ src/test/java/com/ridetogether/chat_service/ChatServiceApplicationTests.java
@@ -1,13 +1,17 @@
 package com.ridetogether.chat_service;
 
+import com.ridetogether.chat_service.config.WebSocketContainerConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootTest
 class ChatServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
